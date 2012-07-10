6bbb5ff3c7fe045f217a5b969fd49fcce5b0fd68
Alex Sinishin <sinishin@gmail.com>
2012-07-10 04:46:07 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truewallet::Application.config.secret_token = 'd24a850ba866eaa05fa804c4f75c4ff599ebf05ceb3d8681b74240d1f9b7bc65b33a60da279dd5b9e8950214efff7e97f27cab8c595238da0a015ef58116a291'
